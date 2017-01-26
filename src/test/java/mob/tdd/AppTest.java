a2c08730ac22c7205abc57c1b5158fc49edd1f3f
Serge Bishyr <seriybg@gmail.com>
2017-01-26 12:41:33 +0200

    Initial commit (create app structure)

---
+++ src/test/java/mob/tdd/AppTest.java
@@ -0,0 +1,20 @@
+package mob.tdd;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
